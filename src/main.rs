8cb652340d8cbee9084bac41a74d93ee3bf6f533
HaoranYi <haoran.yi@solana.com>
2023-10-20 18:25:24 +0000

    up

--- src/main.rs
+++ src/main.rs
@@ -20,4 +20,27 @@ fn main() {
 
     // Print a hash as hex.
     println!("{}", hash1);
+
+    let bytes: Vec<u8> = (0..=255_u8).collect();
+
+    let mut hasher = blake3::Hasher::new();
+    hasher.update(&bytes[0..24]);
+
+    hasher.update(&bytes[24..89]);
+    let h1 = hasher.finalize();
+
+    let mut hasher = blake3::Hasher::new();
+    hasher.update(&bytes[0..8]);
+    hasher.update(&bytes[8..16]);
+    hasher.update(&bytes[16..24]);
+    hasher.update(&bytes[24..25]);
+    hasher.update(&bytes[25..57]);
+    hasher.update(&bytes[57..89]);
+    let h2 = hasher.finalize();
+
+    // Print a hash as hex.
+    println!("{}", h1);
+    println!("{}", h2);
+
+    assert_eq!(h1, h2);
 }
